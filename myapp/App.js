7425eca34f91547acfbd776c683543f46876fd3e
Josh Silverman <jrsilverman71@hotmail.com>
2019-04-17 15:42:19 -0700

    setting up react native and 1st component

---
+++ myapp/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { AppRegistry, Text, View } from "react-native";
+import Component1 from "./components/Component1/Component1";
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <View>
+        <Component1 />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent("MyApp", () => MyApp);
