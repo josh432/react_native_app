f129ac93b9e486339f83a9cc634f2bf0243abc2c
Josh Silverman <jrsilverman71@hotmail.com>
2019-04-18 10:57:05 -0700

    more styling using styleSheet

--- myapp/components/Component2/Component2.js
+++ myapp/components/Component2/Component2.js
@@ -4,8 +4,21 @@ import { AppRegistry, Text, View, StyleSheet } from "react-native";
 export default class Component2 extends Component {
   render() {
     return (
-      <View style={styles.myView}>
-        <Text style={styles.myText}>Hello Josh</Text>
+      <View>
+        <View style={styles.myView}>
+          <Text style={styles.myText}>Hello Josh</Text>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.v1}>
+            <Text>View 1</Text>
+          </View>
+          <View style={styles.v2}>
+            <Text>View 2</Text>
+          </View>
+          <View style={styles.v3}>
+            <Text>View 2</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -18,6 +31,29 @@ const styles = StyleSheet.create({
 
   myText: {
     color: "white"
+  },
+
+  container: {
+    flexDirection: "row",
+    height: 100
+  },
+
+  v1: {
+    flex: 1,
+    backgroundColor: "red",
+    padding: 10
+  },
+
+  v2: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 10
+  },
+
+  v3: {
+    flex: 1,
+    backgroundColor: "black",
+    padding: 10
   }
 });
 
