e516e1870343be38ee621096b8d4a5db36d260f2
Josh Silverman <jrsilverman71@hotmail.com>
2019-04-18 10:38:00 -0700

    displaying component2

--- myapp/App.js
+++ myapp/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { AppRegistry, Text, View } from "react-native";
-import Component1 from "./components/Component1/Component1";
+import Component2 from "./components/Component2/Component2";
 
 export default class MyApp extends Component {
   render() {
     return (
       <View>
-        <Component1 />
+        <Component2 />
       </View>
     );
   }
