fa4e3ea6c33adc4e2954fe5083053fd45057d727
Josh Silverman <jrsilverman71@hotmail.com>
2019-04-18 10:22:37 -0700

    setting state in component

--- myapp/components/Component1/Component1.js
+++ myapp/components/Component1/Component1.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import { AppRegistry, Text, View } from "react-native";
 
 export default class Component1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Josh"
+    };
+  }
+
   render() {
     return (
       <View>
         <Text>{this.props.message}</Text>
+        <Text>{this.state.name}</Text>
       </View>
     );
   }
